c6182b017463d3d600cd3dfb9dc1644a8c2177a9
job963 <jobarthel@gmail.com>
2015-12-16 21:12:53 +0100

    Readme updated

--- copy_this/modules/jxmods/jxadminlog/metadata.php
+++ copy_this/modules/jxmods/jxadminlog/metadata.php
@@ -11,8 +11,12 @@ $aModule = array(
     'id'           => 'jxadminlog',
     'title'        => 'jxAdminLog - Display of Logged Admin Actions',
     'description'  => array(
-                        'de' => 'Anzeige der protokollierten Admin Aktionen an jedem Objekt und als Gesamtbericht.',
-                        'en' => 'Display of Logged Administrative Actions for each Object and as complete Report.'
+                        'de' => 'Anzeige der protokollierten Admin Aktionen an jedem Objekt und als Gesamtbericht.<br /><br />'
+                                . '(um das Logging zu aktivieren muss in der Datei config.inc.php die Einstellung<br />'
+                                . '<code>$this->blLogChangesInAdmin = false;</code> auf <code>True</code> geändert werden)<br /><br />',
+                        'en' => 'Display of Logged Administrative Actions for each Object and as complete Report.<br /><br />'
+                                . '(for enabling the logging you have to change the setting<br />'
+                                . '<code>$this->blLogChangesInAdmin = false;</code> to <code>True</code>)<br /><br />'
                         ),
     'thumbnail'    => 'jxadminlog.png',
     'version'      => '0.1.0',
